2b0d5444dc19302d13d9f7f54e6caa93a9a51bc0
Derrick Dahlitz <ddahlitz@iMac-28.lan>
2022-10-02 15:19:07 -0700

    fixed views page to allow update to work in server

--- boards/urls.py
+++ boards/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
-from .views import BoardDetail, BoardList, ItemList, ItemDetail, ListList, ListDetail
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('api/board', BoardList.as_view(), name='board_list'),
-    path('api/board/<int:pk>/', BoardDetail.as_view(), name='board_detail'),
-    path('api/items/', ItemList.as_view(), name='item_list'),
-    path('api/items/<int:pk>/', ItemDetail.as_view(), name='item_detail'),
-    path('api/lists/', ListList.as_view(), name='list_list'),
-    path('api/lists/<int:pk>/', ListDetail.as_view(), name='list_detail'),
+    path('api/board', views.BoardList.as_view(), name='board_list'),
+    path('api/board/<int:pk>', views.BoardDetail.as_view(), name='board_detail'),
+    path('api/items/', views.ItemList.as_view(), name='item_list'),
+    path('api/items/<int:pk>', views.ItemDetail.as_view(), name='item_detail'),
+    path('api/lists/', views.ListList.as_view(), name='list_list'),
+    path('api/lists/<int:pk>', views.ListDetail.as_view(), name='list_detail'),
 ]
